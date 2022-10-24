ef1b9889e0e0fb68476d40048b383ab3fa3efda0
Miko <jherjati@gmail.com>
2022-10-24 22:43:09 +0700

    add dynamic stats

--- src/components/insight/TableMetric.jsx
+++ src/components/insight/TableMetric.jsx
@@ -1,27 +1,37 @@
 import { useEffect, useState } from "preact/hooks";
-import { dbWorker } from "../../contexts";
-import Charts from "./Charts";
+import { dbWorker, formats } from "../../contexts";
+import ChartItem from "./ChartItem";
 import Stats from "./Stats";
 
 function TableMetric({ name }) {
   const [stats, setStats] = useState([]);
-  const [config, setConfig] = useState({ stats: ["column_4"] });
+  const [config, setConfig] = useState({
+    stats: [
+      formats.value[name].find((col) => ["integer", "real"].includes(col.type))
+        .name,
+    ],
+  });
 
   useEffect(() => {
     dbWorker.value.onmessage = ({ data }) => {
-      console.log(data.results[0].values);
       if (data.id === "get metric") {
-        setStats(data.results[0].values);
+        setStats(
+          data.results
+            .filter((_, idx) => idx < config.stats.length)
+            .map((el) => el.values[0])
+        );
       }
     };
 
     dbWorker.value.postMessage({
       id: "get metric",
       action: "exec",
-      sql: `${config.stats.map(
-        (stat) =>
-          `SELECT MAX(${stat}), AVG(${stat}), MIN(${stat}), COUNT(${stat}) FROM '${name}';`
-      )}`,
+      sql: `${config.stats
+        .map(
+          (stat) =>
+            `SELECT MAX(${stat}), AVG(${stat}), MIN(${stat}), COUNT(${stat}) FROM '${name}';`
+        )
+        .join(" ")}`,
     });
   }, [config]);
 
@@ -33,7 +43,82 @@ function TableMetric({ name }) {
       {stats.map((stat, idx) => (
         <Stats column={config.stats[idx]} values={stat} />
       ))}
-      <Charts />
+      <div className='w-full grid grid-cols-6 pb-6'>
+        <ChartItem
+          config={{
+            type: "line",
+            colors: ["green"],
+            lineOptions: {
+              dotSize: 1.25,
+              regionFill: 1, // default: 1
+            },
+            valuesOverPoints: 1,
+          }}
+          data={{
+            labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            datasets: [{ values: [18, 40, 30, 35, 8, 52, 17, -4] }],
+          }}
+          span={6}
+        />
+        <ChartItem
+          config={{
+            type: "bar",
+            colors: ["orange"],
+            axisOptions: {
+              xAxisMode: "tick",
+              xIsSeries: true,
+            },
+            barOptions: {
+              stacked: true,
+              spaceRatio: 0.5,
+            },
+            valuesOverPoints: 1,
+          }}
+          data={{
+            labels: [
+              "12am-3am",
+              "3am-6am",
+              "6am-9am",
+              "9am-12pm",
+              "12pm-3pm",
+              "3pm-6pm",
+              "6pm-9pm",
+              "9pm-12am",
+            ],
+
+            datasets: [
+              {
+                values: [25, 40, 30, 35, 8, 52, 17, -4],
+              },
+            ],
+          }}
+          span={6}
+        />
+        <ChartItem
+          config={{
+            type: "percentage",
+            valuesOverPoints: 1,
+          }}
+          data={{
+            labels: [
+              "12am-3am",
+              "3am-6am",
+              "6am-9am",
+              "9am-12pm",
+              "12pm-3pm",
+              "3pm-6pm",
+              "6pm-9pm",
+              "9pm-12am",
+            ],
+            datasets: [
+              {
+                values: [25, 40, 30, 35, 8, 52, 17, -4],
+              },
+            ],
+          }}
+          span={6}
+        />
+      </div>
       <hr />
     </section>
   );
--- src/pages/Insight.jsx
+++ src/pages/Insight.jsx
@@ -2,6 +2,7 @@ import { useErrorBoundary } from "preact/hooks";
 import PageError from "../components/core/PageError";
 import TableMetric from "../components/insight/TableMetric";
 import EmptyDb from "../components/manage/EmptyDb";
+import { formats } from "../contexts";
 import { useTables } from "../hooks";
 import { setSnackContent } from "../utils";
 
@@ -27,7 +28,12 @@ function Insight() {
         </div>
         <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>
           {(!dbTables || !dbTables.length) && <EmptyDb />}
-          {dbTables && <TableMetric name={dbTables[0]} />}
+          {dbTables && (
+            <TableMetric
+              name={dbTables[0]}
+              columns={formats.value[dbTables[0]]}
+            />
+          )}
         </div>
       </main>
     );
