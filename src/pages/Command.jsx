0541ff98a28de93cf00cadb6d56760ea22982afa
Miko <jherjati@gmail.com>
2022-11-02 16:22:27 +0700

    add basic sql command area

--- src/pages/Command.jsx
+++ src/pages/Command.jsx
@@ -1,4 +1,25 @@
+import { useCallback, useRef } from "preact/hooks";
+import { EditorView, basicSetup } from "codemirror";
+import { sql } from "@codemirror/lang-sql";
+
 function Command() {
+  const editor = useRef();
+  const ref = useCallback((node) => {
+    editor.current = new EditorView({
+      doc: `
+SELECT 
+  name 
+FROM 
+  sqlite_schema 
+WHERE 
+  type = 'table' 
+  AND name NOT LIKE 'sqlite_%';
+`,
+      extensions: [basicSetup, sql()],
+      parent: node,
+    });
+  }, []);
+
   return (
     <main className='py-6'>
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
@@ -7,7 +28,7 @@ function Command() {
       <div className='mx-auto max-w-7xl px-4 sm:px-6 md:px-8'>
         {/* Replace with your content */}
         <div className='py-4'>
-          <div className='h-96 rounded-lg border-4 border-dashed border-gray-200' />
+          <div ref={ref} className='border bg-white' />
         </div>
         {/* /End replace */}
       </div>
