bae63af08dadaf56dc6f48b1d03e2cb0d8626e52
Miko <jherjati@gmail.com>
2022-11-02 17:05:38 +0700

    add execute function

--- src/pages/Command.jsx
+++ src/pages/Command.jsx
@@ -1,24 +1,42 @@
-import { useCallback, useRef } from "preact/hooks";
+import { useCallback, useRef, useState } from "preact/hooks";
 import { EditorView, basicSetup } from "codemirror";
 import { sql } from "@codemirror/lang-sql";
+import { dbWorker } from "../contexts";
 
 function Command() {
   const editor = useRef();
+  const [results, setResults] = useState([]);
+
   const ref = useCallback((node) => {
-    editor.current = new EditorView({
+    const localEditor = new EditorView({
       doc: `
 SELECT 
-  name 
+  * 
 FROM 
   sqlite_schema 
 WHERE 
   type = 'table' 
   AND name NOT LIKE 'sqlite_%';
+
+PRAGMA table_info('Exception_Report');
 `,
       extensions: [basicSetup, sql()],
       parent: node,
     });
+    editor.current = localEditor;
+
+    dbWorker.value.onmessage = ({ data }) => {
+      setResults(data.results ?? []);
+    };
   }, []);
+
+  const onExecute = () => {
+    dbWorker.value.postMessage({
+      id: "execute command",
+      action: "exec",
+      sql: editor.current.state.doc.toString(),
+    });
+  };
 
   return (
     <main className='py-6'>
@@ -29,6 +47,54 @@ WHERE
         {/* Replace with your content */}
         <div className='py-4'>
           <div ref={ref} className='border bg-white' />
+          <div className='flex justify-center border-t mt-3 pt-3 border-teal-100'>
+            <button
+              onClick={() => {}}
+              className='bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500'
+            >
+              Clear
+            </button>
+            <button
+              onClick={onExecute}
+              className='ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500'
+            >
+              Execute
+            </button>
+          </div>
+          {results.map((res) => (
+            <section
+              style={{ animation: "forwards fadein1 1.6s" }}
+              className='mt-6 p-6 bg-white rounded-lg shadow'
+            >
+              <div className='-mx-4 flex flex-col sm:-mx-6 md:mx-0 overflow-x-scroll'>
+                <table className='min-w-full divide-y divide-gray-300'>
+                  <thead>
+                    <tr>
+                      {res.columns.map((el) => (
+                        <th
+                          scope='col'
+                          className='hidden py-3.5 px-3 text-right text-sm font-semibold text-gray-900 sm:table-cell'
+                        >
+                          {el}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {res.values.map((row) => (
+                      <tr className='border-b border-gray-200'>
+                        {row.map((el) => (
+                          <td className='hidden py-4 px-3 text-right text-sm text-gray-500 sm:table-cell'>
+                            {el}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </section>
+          ))}
         </div>
         {/* /End replace */}
       </div>
