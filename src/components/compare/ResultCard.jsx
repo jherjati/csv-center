f49d07e5522356d61ab4895fd09cbd064cf48d39
Miko <jherjati@gmail.com>
2022-11-14 00:21:29 +0700

    set static table column

--- src/components/compare/ResultCard.jsx
+++ src/components/compare/ResultCard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 import {
   Chart,
   LineController,
@@ -8,6 +8,7 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
+import { dbWorker } from "../../contexts";
 Chart.register(
   LineController,
   LinearScale,
@@ -21,59 +22,59 @@ function ResultCard() {
   const elRef = useRef();
   const chartRef = useRef();
 
-  const data = {
-      labels: [0, 1, 2, 3, 4, 5, 6, 7, 8],
-      datasets: [
-        {
-          label: "data 1",
-          data: [
-            { x: 0, y: 0 },
-            { x: 1, y: 1 },
-            { x: 2, y: 2 },
-            { x: 3, y: 3 },
-            { x: 4, y: 4 },
-            { x: 5, y: 5 },
-            { x: 6, y: 6 },
-            { x: 7, y: 7 },
-            { x: 8, y: 8 },
-          ],
-        },
-        {
-          label: "data 2",
-          data: [
-            { x: 0, y: 4 },
-            { x: 1, y: 4 },
-            { x: 2, y: 4 },
-            { x: 3, y: 4 },
-            { x: 4, y: 4 },
-            { x: 5, y: 4 },
-            { x: 6, y: 4 },
-            { x: 7, y: 4 },
-            { x: 8, y: 4 },
-          ],
-        },
-      ],
-    },
-    config = {
-      type: "line",
-      options: {
-        responsive: true,
-        scales: {
-          x: {
-            type: "linear",
-            title: {
-              display: true,
-              text: "X Axis Label",
-            },
-          },
-        },
-        plugins: {
-          legend: {
-            position: "top",
+  const [data, setData] = useState({
+    datasets: [
+      {
+        label: "data 1",
+        data: [
+          { x: 0, y: 0 },
+          { x: 1, y: 1 },
+          { x: 2, y: 2 },
+          { x: 3, y: 3 },
+          { x: 4, y: 4 },
+          { x: 5, y: 5 },
+          { x: 6, y: 6 },
+          { x: 7, y: 7 },
+          { x: 8, y: 8 },
+        ],
+      },
+      {
+        label: "data 2",
+        data: [
+          { x: 0, y: 4 },
+          { x: 1, y: 4 },
+          { x: 2, y: 4 },
+          { x: 3, y: 4 },
+          { x: 4, y: 4 },
+          { x: 5, y: 4 },
+          { x: 6, y: 4 },
+          { x: 7, y: 4 },
+          { x: 8, y: 4 },
+        ],
+      },
+    ],
+  });
+
+  const config = {
+    type: "line",
+    options: {
+      responsive: true,
+      scales: {
+        x: {
+          type: "linear",
+          title: {
+            display: true,
+            text: "km_hm1",
           },
         },
       },
-    };
+      plugins: {
+        legend: {
+          position: "top",
+        },
+      },
+    },
+  };
 
   useEffect(() => {
     if (elRef.current) {
@@ -88,7 +89,26 @@ function ResultCard() {
     };
   }, [data]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    dbWorker.value.onmessage = ({ data }) => {
+      const datasets = data.results.map(({ columns, values }, idx) => ({
+        label: `exception_report_${idx + 1} (${columns[1]})`,
+        data: values.map((val) => ({ x: val[0], y: val[1] })),
+        parsing: false,
+        normalized: true,
+        borderColor: idx ? "pink" : "orange",
+        backgroundColor: idx ? "red" : "yellow",
+      }));
+      setData({ datasets });
+    };
+
+    const sql = `
+    SELECT km_hm1, panjang_kerusakan FROM exception_report ORDER BY km_hm1 LIMIT 100;
+    SELECT km_hm1, kerusakan_mm FROM exception_report_2 ORDER BY km_hm1 LIMIT 100;
+    `;
+    const params = [];
+    dbWorker.value.postMessage({ id: "compare", action: "exec", params, sql });
+  }, []);
 
   return (
     <section className='my-6 py-3 bg-white shadow rounded-lg'>
