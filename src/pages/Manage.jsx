efd966478edfa8f6bc307211968099c9d5ed072e
Miko <jherjati@gmail.com>
2022-11-26 23:38:00 +0700

    prioritize newest table at manage page

--- src/pages/Manage.jsx
+++ src/pages/Manage.jsx
@@ -1,15 +1,35 @@
-import { useErrorBoundary, useState } from "preact/hooks";
+import { useEffect, useErrorBoundary, useState } from "preact/hooks";
 import PageError from "../components/core/PageError";
 import DbTable from "../components/manage/DbTable";
 import EmptyDb from "../components/core/EmptyDb";
 import { formats } from "../contexts";
-import { useTables } from "../hooks";
 import { setSnackContent } from "../utils";
 import SampleLoader from "../components/core/SampleLoader";
+import { DBWorker } from "../constants";
 
 function Manage() {
-  const { dbTables } = useTables();
+  const [dbTables, setDbTables] = useState();
   const [activeTable, setActiveTable] = useState(0);
+
+  useEffect(() => {
+    DBWorker.pleaseDo({
+      id: "browse table",
+      action: "exec",
+      sql: `SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';`,
+    })
+      .then(({ results }) => {
+        if (results) {
+          const newTables = results[0]?.values?.map((el) => el[0]);
+          setDbTables(newTables);
+          const tableName = new URLSearchParams(window.location.search).get(
+            "table"
+          );
+          if (tableName)
+            setActiveTable(newTables.findIndex((name) => name === tableName));
+        }
+      })
+      .catch(console.error);
+  }, []);
 
   const [error, resetError] = useErrorBoundary((error) => {
     console.error(error);
